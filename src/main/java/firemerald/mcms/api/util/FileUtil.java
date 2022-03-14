ad6dd8dd1d9b4591c36833bece9d33c15797aa70
FirEmerald <mccaslinwalt@gmail.com>
2022-03-14 15:25:18 -0500

    remove GSON as it doesn't work on Java 9 or newer

--- src/main/java/firemerald/mcms/api/util/FileUtil.java
+++ src/main/java/firemerald/mcms/api/util/FileUtil.java
@@ -3,17 +3,10 @@ package firemerald.mcms.api.util;
 import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.Reader;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.Iterator;
-import java.util.Map.Entry;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -30,13 +23,6 @@ import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
-import com.google.gson.internal.bind.TypeAdapters;
-import com.google.gson.stream.JsonReader;
 import firemerald.mcms.api.API;
 import firemerald.mcms.api.data.*;
 import firemerald.mcms.gui.popups.GuiPopupException;
@@ -47,7 +33,6 @@ public class FileUtil
 	public static final DocumentBuilder DB;
     public static final TransformerFactory TFF = TransformerFactory.newInstance();
     public static final Transformer TF;
-	public static final Gson GSON = new Gson();
 	
     static
 	{
@@ -118,14 +103,9 @@ public class FileUtil
     	 * <(name)>
     	 *     <(child)/>
     	 * </(name)>
-    	 * 
+    	 *
     	 * Binary: it's custom binary, no text representation or data conversion required.
-    	 * 
-    	 * JSON:
-    	 * {#name:(name),#value:(value),(attribute):(value),(child):{},(child):[0:(value),1:(value)],(child):[0:{},1:{}]}
-    	 * 
-    	 * There is a rudimentary JSON saver in place, but it won't handle object names with non-alphanumeric characters. If you need those, use XML as it is far better than JSON anyway.
-    	 * 
+    	 *
     	 * if there is no '#name', the name is 'root' for the root object or the name used to define it e.g. 'child:{}' would be named 'child'
     	 * if there is no '#value' the value is null - conversely, for re-encoding if the value is null there will be no '#value' present.
     	 * if the value is null, there are no children, and the attributes' names are all positive integers in sequence, it's a value array.
@@ -139,7 +119,7 @@ public class FileUtil
     	 *     double
     	 *     string
     	 * if the value is null, there are no attributes, and all childrens' names are all positive integers in sequence, it's an object array.
-    	 * 
+    	 *
     	 */
     	AbstractElement el = null;
     	try
@@ -160,16 +140,13 @@ public class FileUtil
 			if (el == null) switch (header[0])
 			{
 			// 00 3C 00 3F = UTF16BE XML
-			// 00 5B xx xx = UTF16BE JSON
-			// 00 7B xx xx = UTF16BE JSON
 			case 0:
-				if (length >= 2)
+				if (length >= 4 && 
+				header[1] == 0x3C && 
+				header[2] == 0x00 && 
+				header[3] == 0x3F)
 				{
-					if (length >= 4 && header[1] == 0x3C)
-					{
-						if (header[2] == 0x00 && header[3] == 0x3F) el = loadXML(buffered);
-					}
-					else if (header[1] == 0x5B || header[1] == 0x7B) el = loadJSON(buffered, StandardCharsets.UTF_16BE);
+					el = loadXML(buffered);
 				}
 				break;
 			// 3C 00 3F 00 = UTF16LE XML
@@ -184,49 +161,25 @@ public class FileUtil
 					else if (header[1] == 0x3F && header[2] == 0x78 && header[3] == 0x6D) el = loadXML(buffered);
 				}
 				break;
-			// 5B 00 xx xx = UTF16LE JSON
-			// 5B xx xx xx = UTF8 JSON
-			// 7B 00 xx xx = UTF16LE JSON
-			// 7B xx xx xx = UTF8 JSON
-			case 0x5B:
-			case 0x7B:
-				if (length >= 2 && header[1] == 0x00) el = loadJSON(buffered, StandardCharsets.UTF_16LE);
-				else el = loadJSON(buffered, StandardCharsets.UTF_8);
-				break;
-			// EF BB BF 5B = UTF8 JSON, skip 3
-			// EF BB BF 7B = UTF8 JSON, skip 3
 			// EF BB BF xx = UTF8 XML, skip 3
 			case (byte) 0xEF:
 				if (length >= 3 && header[1] == (byte) 0xBB && header[2] == (byte) 0xBF)
 				{
-					if (length >= 4 && (header[3] == 0x5B || header[3] == 0x7B)) el = loadJSON(buffered, StandardCharsets.UTF_8);
-					else el = loadXML(buffered);
+					el = loadXML(buffered);
 				}
 				break;
-			// FE FF 00 5B = UTF16BE JSON, skip 2
-			// FE FF 00 7B = UTF16BE JSON, skip 2
 			// FE FF xx xx = UTF16BE XML, skip 2
 			case (byte) 0xFE:
 				if (length >= 2 && header[1] == (byte) 0xFF)
 				{
-					if (length >= 4 && header[2] == 0x00)
-					{
-						if (header[3] == 0x5B || header[3] == 0x7B) el = loadJSON(buffered, StandardCharsets.UTF_16BE);
-					}
-					else el = loadXML(buffered);
+					el = loadXML(buffered);
 				}
 				break;
-			// FF FE 5B 00 = UTF16LE JSON, skip 2
-			// FF FE 7B 00 = UTF16LE JSON, skip 2
 			// FF FE xx xx = UTF16LE XML, skip 2
 			case (byte) 0xFF:
 				if (length >= 2 && header[1] == (byte) 0xFE)
 				{
-					if (length >= 4 && header[3] == 0x00)
-					{
-						if (header[2] == 0x5B || header[2] == 0x7B) el = loadJSON(buffered, StandardCharsets.UTF_16LE);
-					}
-					else el = loadXML(buffered);
+					el = loadXML(buffered);
 				}
 				break;
 			}
@@ -246,7 +199,7 @@ public class FileUtil
 		closeSafe(in);
     	return el;
     }
-    
+
     public static AbstractElement loadXML(InputStream in) throws SAXException, IOException
     {
 		Document doc = readXML(in);
@@ -257,29 +210,6 @@ public class FileUtil
     {
 		in.read();
 		return Element.loadBinaryHeaderless(in, format);
-    }
-    
-    public static Element loadJSON(InputStream in, Charset charset) throws IOException, IllegalArgumentException
-    {
-    	return loadJSON(in, charset, true);
-    }
-    
-    public static Element loadJSON(InputStream in, Charset charset, boolean lenient) throws IOException, IllegalArgumentException
-    {
-    	return loadJSON(new InputStreamReader(in, charset), lenient);
-    }
-    
-    public static Element loadJSON(Reader reader) throws IOException, IllegalArgumentException
-    {
-    	return loadJSON(reader, true);
-    }
-    
-    public static Element loadJSON(Reader reader, boolean lenient) throws IOException, IllegalArgumentException
-    {
-    	JsonReader jsonReader = new JsonReader(reader);
-    	jsonReader.setLenient(lenient);
-    	JsonElement element = TypeAdapters.JSON_ELEMENT.read(jsonReader);
-    	return Element.loadJSON(element);
     }
 	
 	public static Document readXML(InputStream in) throws SAXException, IOException
@@ -315,29 +245,6 @@ public class FileUtil
 	    DOMSource source = new DOMSource(doc);
 	    StreamResult result = new StreamResult(System.out);
 		TF.transform(source, result);
-	}
-	
-	public static void saveJSON(JsonElement el, File output) throws IOException
-	{
-		if (output.getParentFile() != null) output.getParentFile().mkdirs();
-		output.createNewFile();
-		OutputStream out = null;
-		try
-		{
-			out = new FileOutputStream(output);
-			saveJSON(el, out);
-			out.close();
-		}
-		catch (IOException e)
-		{
-			FileUtil.closeSafe(out);
-			throw e;
-		}
-	}
-	
-	public static void saveJSON(JsonElement el, OutputStream out) throws IOException
-	{
-		out.write(toString(el, false).getBytes(StandardCharsets.UTF_8));
 	}
 	
 	public static void writeString(OutputStream out, String val, BinaryFormat format) throws IOException
@@ -451,71 +358,6 @@ public class FileUtil
 		}
 	}
 	
-	public static String toString(JsonElement json, boolean oneLine)
-	{
-		StringBuilder str = new StringBuilder();
-		toString(json, str, "", true, oneLine);
-		return str.toString();
-	}
-	
-	private static void toString(JsonElement json, StringBuilder builder, String tab, boolean isLast, boolean oneLine)
-	{
-		if (json.isJsonObject())
-		{
-			JsonObject obj = json.getAsJsonObject();
-			builder.append('{');
-			if (!oneLine) builder.append('\n');
-			Iterator<Entry<String, JsonElement>> entries = obj.entrySet().iterator();
-			String newTab = tab + "\t";
-			for (int i = 1; i <= obj.size(); i++)
-			{
-				Entry<String, JsonElement> entry = entries.next();
-				if (!oneLine) builder.append(newTab);
-				builder.append(entry.getKey());
-				builder.append(": ");
-				toString(entry.getValue(), builder, newTab, i == obj.size(), oneLine);
-			}
-			if (!oneLine) builder.append(tab);
-			builder.append('}');
-		}
-		else if (json.isJsonArray())
-		{
-			JsonArray array = json.getAsJsonArray();
-			builder.append('[');
-			if (!oneLine) builder.append('\n');
-			Iterator<JsonElement> values = array.iterator();
-			String newTab = tab + "\t";
-			for (int i = 1; i <= array.size(); i++)
-			{
-				if (!oneLine) builder.append(newTab);
-				toString(values.next(), builder, newTab, i == array.size(), oneLine);
-			}
-			if (!oneLine) builder.append(tab);
-			builder.append(']');
-		}
-		else if (json.isJsonPrimitive())
-		{
-			JsonPrimitive prim = json.getAsJsonPrimitive();
-			if (prim.isString()) builder.append(prim.toString());
-			else if (prim.isBoolean()) builder.append(prim.getAsBoolean() ? "1b" : "0b");
-			else if (prim.isNumber())
-			{
-				Number num = prim.getAsNumber();
-				if (num instanceof Byte) builder.append(num.toString() + "b");
-				else if (num instanceof Short) builder.append(num.toString() + "s");
-				else if (num instanceof Integer) builder.append(num.toString());
-				else if (num instanceof Long) builder.append(num.toString() + "l");
-				else if (num instanceof Float) builder.append(num.toString() + "f");
-				else if (num instanceof Double) builder.append(num.toString() + "d");
-				else builder.append("<INVALID VALUE>");
-			}
-			else builder.append("<INVALID VALUE>");
-		}
-		else builder.append("<INVALID VALUE>");
-		if (!isLast) builder.append(", ");
-		if (!oneLine) builder.append('\n');
-	}
-	
 	public static String getExtension(String fileName)
 	{
 	    char ch;
@@ -562,19 +404,6 @@ public class FileUtil
 			{
 				W3CElement wc3 = W3CElement.convert(el);
 				wc3.save(file);
-			}
-		},
-		JSON("json") {
-			@Override
-			public AbstractElement newElement(String name)
-			{
-				return new Element(name);
-			}
-			
-			@Override
-			public void saveElement(AbstractElement el, File file) throws Exception
-			{
-				FileUtil.saveJSON(el.toElement().makeElement(true), file);
 			}
 		},
 		BINARY("bin") {
